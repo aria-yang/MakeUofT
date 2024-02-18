4fd191ee4a0dfc02569a540dfbe1b9286e5ddc86
aria-yang <146586541+aria-yang@users.noreply.github.com>
2024-02-17 21:28:48 -0500

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -2,17 +2,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <Arduino.h>
-#include <U8g2lib.h>
 
-#ifdef U8X8_HAVE_HW_SPI
-#include <SPI.h>
-#endif
-#ifdef U8X8_HAVE_HW_I2C
-#include <Wire.h>
-#endif
-
-U8G2_SSD1306_128X32_UNIVISION_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // Adafruit Feather ESP8266/32u4 Boards + FeatherWing OLED
 
 #define SCREEN_I2C_ADDR 0x3D
 #define SCREEN_WIDTH 128
@@ -48,16 +38,16 @@ void setup() {
   // put your setup code here, to run once:
 
   // set up display
-  // if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-  // Serial.println(F("SSD1306 allocation failed"));
-  // while (true);
-  // }
-  // oled.clearDisplay();
-  // oled.setTextSize(1);
-  // oled.setTextColor(SSD1306_WHITE);
-  // oled.setCursor (0, 0);
-  // oled.println("This works!");
-
+  if (!oled.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+  Serial.println(F("SSD1306 allocation failed"));
+  while (true);
+  }
+  oled.clearDisplay();
+  oled.setTextSize(1);
+  oled.setTextColor(SSD1306_WHITE);
+  oled.setCursor (0, 0);
+  oled.print("");
+  oled.display();
   // set LED as outputs
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
@@ -67,8 +57,6 @@ void setup() {
   
   // initialize serial communication
   Serial.begin(9600);
-
-  u8g2.begin();
 
   // #ifndef ESP8266
   // while (!Serial); // wait for serial port to connect. Needed for native USB
@@ -87,21 +75,20 @@ void loop() {
   sensorValue = analogRead(sensorPin);
   // DisplayDateTime(); 
 
-  u8g2.clearBuffer();					// clear the internal memory
-  u8g2.setFont(u8g2_font_ncenB08_tr);	// choose a suitable font
-  u8g2.drawStr(0,10, sensorValue);	// write something to the internal memory
-  u8g2.sendBuffer();					// transfer internal memory to the display
-
   // Oled.setFont(u8x8_font_chroma48medium8_r);
   // Oled.setCursor(0, 33);
   // Oled.print("Analog Value:");
   // Oled.print(sensorValue);
   // Oled.refreshDisplay();
   // delay(1000);
-  
+  oled.clearDisplay();
+  oled.setCursor(0, 0);
+  oled.print("Sensor Value: ");
+  oled.println(sensorValue);
+  oled.display();
   // print sensor value
-  // Serial.println("Sensor Value: ");
-  // Serial.println(sensorValue);
+  Serial.print("Sensor Value: ");
+  Serial.println(sensorValue);
 
   // LED directions
   if(sensorValue > 750) {
@@ -116,7 +103,6 @@ void loop() {
     digitalWrite(led1, LOW);
     digitalWrite(led2, LOW);
   }
-  delay(200);
 }
 
 // void DisplayDateTime()
